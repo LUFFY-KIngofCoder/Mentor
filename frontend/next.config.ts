3fa48d7f2af5f36a99cd56a03cfc0752776b7891
LUFFY-KIngofCoder <shashankghs999@gmail.com>
2026-08-18 00:52:11 +0530

    fix: allow AWS IP in Next allowedDevOrigins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.9'],
+  allowedDevOrigins: ['192.168.1.9', '65.2.70.233'],
 };
 
 export default nextConfig;
