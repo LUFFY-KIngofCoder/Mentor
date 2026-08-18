8105d092c03565f1a885565a299725f7ceec00d7
LUFFY-KIngofCoder <shashankghs999@gmail.com>
2026-08-18 23:48:36 +0530

    feat: add nginx reverse proxy on port 80

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -13,7 +13,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","http://65.2.70.233:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://65.2.70.233:3000",
+        "http://localhost",
+        "http://65.2.70.233"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
